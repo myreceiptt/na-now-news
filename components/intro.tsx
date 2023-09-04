052fa6905cb769ca58d1296f998f7f1d3418b874
myreceiptt <nota@straight-line.org>
2023-09-05 04:42:03 +0700

    update!

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,14 +7,14 @@ export default function Intro() {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        Slow but surely updated by the{' '}
         <a
-          href="https://nextjs.org/"
+          href="https://www.bananow.land/#farmers"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          Next.js
+          Farmers
         </a>{' '}
-        and{' '}
+        of{' '}
         <a
           href={CMS_URL}
           className="underline hover:text-success duration-200 transition-colors"
