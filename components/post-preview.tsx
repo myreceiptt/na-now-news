149b479518021de6ce629ee79bedf604eb6ae886
myreceiptt <nota@straight-line.org>
2023-09-08 19:13:04 +0700

    Uppercase to Lowercase

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,6 +1,6 @@
-import Avatar from './Avatar'
-import Date from './Date'
-import CoverImage from './CoverImage'
+import Avatar from './avatar'
+import Date from './date'
+import CoverImage from './cover-image'
 import Link from 'next/link'
 
 export default function PostPreview({
