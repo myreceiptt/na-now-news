72a049470d10fe61e6a39261fc60ce69f0526119
myreceiptt <nota@straight-line.org>
2023-09-09 17:52:46 +0700

    okehs

--- components/alertt.tsx
+++ components/alertt.tsx
@@ -5,7 +5,7 @@ import { EXAMPLE_PATH } from '../lib/constants'
 export default function Alert({ preview }) {
   return (
     <Container>
-      <div className="alert border-info mt-2">
+      <div className="alert border-info text-sm lg:text-base mt-2">
         {preview ? (
           <>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="sm:block hidden stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
--- components/intro.tsx
+++ components/intro.tsx
@@ -4,15 +4,15 @@ export default function Intro() {
   return (
     <>
       <section className="flex flex-col sm:flex-row items-center sm:justify-between mt-8 mb-8">
-        <h1 className="text-5xl md:text-6xl font-bold sm:text-left text-center tracking-tighter">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold sm:text-left text-center tracking-tighter">
           Na Now News
         </h1>
-        <h4 className="text-center sm:text-right text-lg mt-5">
+        <h4 className="text-center sm:text-right text-sm lg:text-base mt-5">
           Slow but surely updated by the{' '}
           <a
             href="https://www.bananow.land/#farmers"
             target="_blank"
-            className="underline hover:text-success duration-200 transition-colors"
+            className="text-primary hover:text-secondary"
           >
             Farmers
           </a>{' '}
@@ -20,7 +20,7 @@ export default function Intro() {
           <a
             href={CMS_URL}
             target="_blank"
-            className="underline hover:text-success duration-200 transition-colors"
+            className="text-primary hover:text-secondary"
           >
             {CMS_NAME}
           </a>
