8c5baccb207fc9993e2b36bc59eb38648cd8b77d
myreceiptt <nota@straight-line.org>
2024-09-30 11:06:03 +0700

    partnership-form

--- src/app/partnership/page.tsx
+++ src/app/partnership/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { compareDesc, format, parseISO } from "date-fns";
-import { allPosts } from "contentlayer/generated";
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 
