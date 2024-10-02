4d7e910ee932ede6698eef1366b8a187fa4b3b4e
myreceiptt <nota@straight-line.org>
2024-10-02 08:21:09 +0700

    custom link

--- src/app/components/customlink.tsx
+++ src/app/components/customlink.tsx
@@ -2,11 +2,17 @@ import Link from "next/link";
 import { HTMLProps } from "react";
 
 export default function CustomLink(
-  props: { href: string } & HTMLProps<HTMLAnchorElement>
+  props: { href?: string } & HTMLProps<HTMLAnchorElement>
 ) {
   // export default function CustomLink(props: { href: string; children: React.ReactNode }) {
 
   const href = props.href;
+
+  if (!href) {
+    console.error("Missing 'href' in CustomLink");
+    return <span>{props.children}</span>;
+  }
+
   const isInternalLink = href && href.startsWith("/");
   const isImageLink = href && href.startsWith("/images");
 
@@ -14,5 +20,11 @@ export default function CustomLink(
     return <Link href={href}>{props.children}</Link>;
   }
 
-  return <a target="_blank" className="text-green-now dark:text-yellow-now hover:text-yellow-now dark:hover:text-green-now underline underline-offset-4" {...props} />;
+  return (
+    <a
+      target="_blank"
+      className="text-green-now dark:text-yellow-now hover:text-yellow-now dark:hover:text-green-now underline underline-offset-4"
+      {...props}
+    />
+  );
 }
