787e2d16a4223bf7d66e8c8766865602e701f062
myreceiptt <nota@straight-line.org>
2024-10-03 02:06:01 +0700

    kehancuran bumi

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -3,5 +3,6 @@
 import allEnergyGenerationWillBeSolarMdx from './all-energy-generation-will-be-solar.mdx.json' assert { type: 'json' }
 import fruitThatConnectsUsMdx from './fruit-that-connects-us.mdx.json' assert { type: 'json' }
 import naNowNewsMdx from './na-now-news.mdx.json' assert { type: 'json' }
+import manusiaDanPotensiKehancuranBumiMdx from './manusia-dan-potensi-kehancuran-bumi.mdx.json' assert { type: 'json' }
 
-export const allPosts = [allEnergyGenerationWillBeSolarMdx, fruitThatConnectsUsMdx, naNowNewsMdx]
+export const allPosts = [allEnergyGenerationWillBeSolarMdx, fruitThatConnectsUsMdx, naNowNewsMdx, manusiaDanPotensiKehancuranBumiMdx]
